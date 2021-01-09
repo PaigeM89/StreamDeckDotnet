7befb5aa9c3c2d2c4f387a870b83e0786feb375b
Max Paige <max.paige@binarydefense.com>
2021-01-09 10:50:08 -0500

    Example project logs

--- ExampleProject/Example/Program.fs
+++ ExampleProject/Example/Program.fs
@@ -3,19 +3,40 @@
 open System
 open StreamDeckDotnet.Websockets
 open StreamDeckDotnet.Engine
+open StreamDeckDotnet.Logging
+open Serilog
+// open Serilog.Sinks.File
+// open Serilog.Sinks.Console
+
+let log = 
+  LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .WriteTo.File("log.txt")
+    .WriteTo.Console()
+    .Enrich.FromLogContext()
+    .CreateLogger()
+
+LogProvider.setLoggerProvider (Providers.SerilogProvider.create ())
+Serilog.Log.Logger <- log
+
+let logger = LogProvider.getLoggerByName("Example.Program")
 
 [<EntryPoint>]
 let main argv =
-    let routes = ExampleProject.Routing.routes
-    let args = {
-      StreamDeckSocketArgs.Port = 0
-      Id = System.Guid.Empty
-      RegisterEvent = ""
-      Info = ""
-    }
+  Log.setMessage("Starting example project") |> logger.trace
+  let routes = ExampleProject.Routing.routes
+  let args = {
+    StreamDeckSocketArgs.Port = 0
+    Id = System.Guid.Empty
+    RegisterEvent = ""
+    Info = ""
+  }
 
-    let client = StreamDeckClient(args, routes)
-    client.Run()
+  Log.setMessage("Creating client") |> logger.trace
+  let client = StreamDeckClient(args, routes)
+  Log.setMessage("Running client") |> logger.trace
+  client.Run()
 
-    printfn "Exiting client.run, exiting program"
-    0 // return an integer exit code
+  printfn "Exiting client.run, exiting program"
+  //log.CloseAndFlush()
+  0 // return an integer exit code
--- src/StreamDeckDotnet/Websockets.fs
+++ src/StreamDeckDotnet/Websockets.fs
@@ -40,12 +40,22 @@ module Websockets =
       let _semaphore = new SemaphoreSlim(1)
       let port = args.Port
 
-      let lockAsync() = _semaphore.WaitAsync() |> Async.AwaitTask
-      let release() = _semaphore.Release()
+      let lockAsync() = 
+        !! "Locking socket" |> logger.info
+        _semaphore.WaitAsync() |> Async.AwaitTask
+      let release() = 
+        !! "Unlocking socket" |> logger.info
+        _semaphore.Release()
 
-      let connectWebsocket port = task {
+      let connectWebsocket (port : int) = task {
         if _websocket.State <> WebSocketState.Open then
-          do!  _websocket.ConnectAsync(Uri($"ws://localhost:{port}"), _cancelSource.Token)
+          let uri = $"ws://localhost:{port}"
+          !! "Connecting web socket to port {port} with uri {uri}"
+          >>!- ("port", port)
+          >>!- ("uri", uri)
+          |> logger.info
+          do!  _websocket.ConnectAsync(Uri(uri), _cancelSource.Token)
+          !! "Finished connecting web socket" |> logger.trace
         else
           ()
       }
@@ -53,33 +63,68 @@ module Websockets =
       let eventsEncoded (ctx : Context.EventContext) =
         ctx.GetEventsToSend()
         |> List.map (fun payloads -> 
-          payloads.Encode ctx.EventMetadata.Context ctx.EventMetadata.Device
+          let payload = payloads.Encode ctx.EventMetadata.Context ctx.EventMetadata.Device
+          !! "Created event sent paylod of {payload}"
+          >>!- ("payload", payload)
+          |> logger.debug
+          payload
         )
+
+      let closeSocket token = 
+        try
+          if isNull _websocket then !! "how did we get here" |> logger.error
+          _websocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Disconnecting", token)
+        with
+        | e -> 
+          !! "Error closing web socket: {msg}"
+          >>!+ ("msg", e.Message)
+          >>!! e
+          |> logger.error
+          Task.FromException e
 
       member this.DisconnectAsync() = async {
           try
-            do! lockAsync()
-            do! _websocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Disconnecting", _cancelSource.Token) |> Async.AwaitTask
-            _websocket.Dispose()
-            _websocket <- new ClientWebSocket()
+            if not (isNull _websocket) && _websocket.State <> WebSocketState.Closed then
+              !! "attempting disconnect socket, currently has state {state}"
+              >>!+ ("state", _websocket.State)
+              |> logger.debug
+              do! lockAsync()
+              !! "Closing socket" |> logger.info
+              let token = _cancelSource.Token
+              //do! _websocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Disconnecting", token) |> Async.AwaitTask
+              do! closeSocket token |> Async.AwaitTask
+              !! "Disposing socket" |> logger.debug
+              _websocket.Dispose()
+              !! "Initializing new websocket" |> logger.trace
+              _websocket <- new ClientWebSocket()
+            else
+              !! "Websocket is attempting to disconnect, but is null"
+              |> logger.warn
           finally
             do release |> ignore
         }
 
       member this.SendToSocketAsync(text : string) = async {
         try
-          if _websocket.State = WebSocketState.Open then
-            do! _semaphore.WaitAsync() |> Async.AwaitTask
-            let buf = Encoding.UTF8.GetBytes(text)
-            do! _websocket.SendAsync(ArraySegment<_>(buf), WebSocketMessageType.Text, true, _cancelSource.Token) |> Async.AwaitTask
-            return ()
-          else
-            return ()
+          try
+            if _websocket.State = WebSocketState.Open then
+              do! _semaphore.WaitAsync() |> Async.AwaitTask
+              !! "Sending {text} to socket" >>!- ("text", text) |> logger.info
+              let buf = Encoding.UTF8.GetBytes(text)
+              do! _websocket.SendAsync(ArraySegment<_>(buf), WebSocketMessageType.Text, true, _cancelSource.Token) |> Async.AwaitTask
+              return ()
+            else
+              return ()
+          with
+          | e ->
+            !! "Received error {e} when sending to socket" >>!- ("e", e.Message) >>!! e |> logger.error
+            ()
         finally
           _semaphore.Release() |> ignore
       }
 
       member this.SendAllToSocketAsync(payloads : string list) = async {
+        !! "Sending multiple payloads to socket" |> logger.trace
         let allAsyncs = payloads |> List.map this.SendToSocketAsync
         do! Async.Sequential allAsyncs |> Async.Ignore
         return ()
@@ -91,13 +136,17 @@ module Websockets =
         let textBuf = StringBuilder(BufferSize)
         async {
           while not _cancelSource.IsCancellationRequested do
+            !! "Awaiting socket receive" |> logger.trace
             let! websocketResult = _websocket.ReceiveAsync(arrayBuf, _cancelSource.Token) |> Async.AwaitTask
             if websocketResult.MessageType = WebSocketMessageType.Text then
+              !! "Received text message from web socket" |> logger.trace
               textBuf.Append(Encoding.UTF8.GetString(buf, 0, websocketResult.Count)) |> ignore
               printfn "Web socket recieved: %s" (string textBuf)
 
-              if websocketResult.EndOfMessage then 
-                let! ctx = receiveHandler (string textBuf)
+              if websocketResult.EndOfMessage then
+                let msg = string textBuf
+                !! "Received message of {msg} from web socket" >>!- ("msg", msg) |> logger.info
+                let! ctx = receiveHandler (msg)
                 do! ctx |> eventsEncoded |> this.SendAllToSocketAsync
                 textBuf.Clear() |> ignore
               ()
@@ -110,12 +159,17 @@ module Websockets =
             do! this.DisconnectAsync()
           }
         let work = async {
+            !! "Starting web socket, connecting... " |> logger.trace
             do! (connectWebsocket port) |> Async.AwaitTask
             if _websocket.State <> WebSocketState.Open then
+              !! "Websocket is not open after starting" |> logger.trace
               ()
             else
+              !! "Web socket successfully connected, sending response" |> logger.trace
               do! this.SendToSocketAsync("response from web socket!")
+            !! "this.Receive() call in runAsync" |> logger.trace
             do! this.Receive()
+            !! "Receive() received, returning in work func" |> logger.trace
             return ()
           }
         Async.tryFinally fin work
